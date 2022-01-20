f4992d4c16c7a9125f6dbbfe45ca7aa089e84adc
amina-seraoui <amina.seraoui@outlook.fr>
2022-01-20 20:38:38 +0100

    :hammer: update Stars.php

--- src/Controller/Api/Stars.php
+++ src/Controller/Api/Stars.php
@@ -9,13 +9,9 @@ class Stars extends Controller
     public function __invoke()
     {
         header('Content-Type: application/json');
-        header('Access-Control-Allow-Origin : *', false);
-        return $this->index();
-    }
-
-    public function index()
-    {
+        // header('Access-Control-Allow-Origin : *', false); // only on dev
         $json = [];
+        
         if (isset($_POST['note'], $_POST['recipe_id'], $_SERVER['REMOTE_ADDR'])) {
             $note = $_POST['note'];
 
@@ -25,44 +21,75 @@ class Stars extends Controller
                 $req->bindParam('id', $_POST['recipe_id'], \PDO::PARAM_INT);
                 $success = $req->execute();
 
-                if ($success && $req->rowCount() === 0) {
-                    try {
-                        $req = $this->pdo->prepare('INSERT INTO stars (ip_address, recipe_id, note) VALUES (:ip, :id, :note)');
-                        $req->bindParam('ip', $_SERVER['REMOTE_ADDR'], \PDO::PARAM_STR);
-                        $req->bindParam('id', $_POST['recipe_id'], \PDO::PARAM_INT);
-                        $req->bindParam('note', $note, \PDO::PARAM_INT);
-                        $success = $req->execute();
-                        $json['success']['req'] = $success;
-                        $json['success']['msg'] = 'Vous avez ajouté la note de ' . (int)$note . ' à la recette !';
-
-                        if ($success) {
-                            $req = $this->pdo->prepare('SELECT CEIL(AVG(note)) note FROM stars WHERE recipe_id = :id GROUP BY recipe_id');
-                            $req->bindParam('id', $_POST['recipe_id'], \PDO::PARAM_INT);
-                            $req->execute();
-                            $json['success']['note'] = $req->fetch(\PDO::FETCH_OBJ)->note;
-                        }
-                    } catch(\PDOException $e) {
-                        if ($e->getCode() === '23000') {
-                            $json['error']['msg'] = (int)$_POST['recipe_id'] . ' n\'est pas un identifiant valide';
-                        } else {
-                            $json['error']['msg'] = 'Une erreur est survenue : ' . $e->getCode();
-                            // $json['error']['msg'] = $e->getMessage();
-                        }
+                try {
+                    if ($success && $req->rowCount() === 0) {
+                        $json = $this->add($_SERVER['REMOTE_ADDR'], (int)$_POST['recipe_id'], (int)$note);                  
+                    } else if ($success && $req->rowCount() === 1) {
+                        $json = $this->update($_SERVER['REMOTE_ADDR'], (int)$_POST['recipe_id'], (int)$note); 
+                    } else {
+                        $json['error'] = [
+                            'req' => $success,
+                            'count' => $req->rowCount(),
+                            'recipe' => $_POST['recipe_id'],
+                            'msg' => 'Impossible d\'ajouter une note'
+                        ];
                     }
-                } else {
-                    $json['error']['recipe'] = $_POST['recipe_id'];
-                    $json['error']['req'] = $success;
-                    $json['error']['count'] = $req->rowCount();
-                    $json['error']['msg'] = 'Impossible d\'ajouter une note';
+
+                    if ($json['success']['req']) {
+                        $req = $this->pdo->prepare('SELECT CEIL(AVG(note)) note FROM stars WHERE recipe_id = :id GROUP BY recipe_id');
+                        $req->bindParam('id', $_POST['recipe_id'], \PDO::PARAM_INT);
+                        $req->execute();
+                        $json['success']['note'] = $req->fetchColumn();
+                    }
+                } catch(\PDOException $e) {
+                    if ($e->getCode() === '23000') {
+                        $json['error']['msg'] = (int)$_POST['recipe_id'] . ' n\'est pas un identifiant valide';
+                    } else {
+                        $json['error']['msg'] = 'Une erreur est survenue : ' . $e->getCode();
+                        // $json['error']['msg'] = $e->getMessage();
+                    }
                 }
             } else {
-                $json['error'] = 'Note invalide';
+                $json['error']['msg'] = 'Note invalide';
             }
-
         } else {
-            $json['error'] = 'Données incomplètes';
+            $json['error']['msg'] = 'Données incomplètes';
         }
 
         return json_encode($json);
+    }
+
+    private function add(string $ip, int $recipe, int $note): array
+    {
+        $json = [];
+
+        $req = $this->pdo->prepare('INSERT INTO stars (ip_address, recipe_id, note) VALUES (:ip, :id, :note)');
+        $req->bindParam('ip', $ip, \PDO::PARAM_STR);
+        $req->bindParam('id', $recipe, \PDO::PARAM_INT);
+        $req->bindParam('note', $note, \PDO::PARAM_INT);
+        $success = $req->execute();
+        $json['success'] = [
+            'req' => $success,
+            'msg' => 'Vous avez ajouté la note de ' . $note . ' à cette recette !'
+        ];
+
+        return $json;
+    }
+
+    private function update(string $ip, int $recipe, int $note): array
+    {
+        $json = [];
+     
+        $req = $this->pdo->prepare('UPDATE stars SET note = :note WHERE ip_address = :ip AND recipe_id = :id');
+        $req->bindParam('note', $note, \PDO::PARAM_INT);
+        $req->bindParam('ip', $ip, \PDO::PARAM_STR);
+        $req->bindParam('id', $recipe, \PDO::PARAM_INT);
+        $success = $req->execute();
+        $json['success'] = [
+            'req' => $success,
+            'msg' => 'Vous avez modifié votre note par ' . $note . ' à cette recette !'
+        ];
+     
+        return $json;
     }
 }
